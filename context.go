4b70bde494d3a0a9cb02192eebf29adb60c36f6c
sunzhaoping <sunzhaoping@sunzhaopingdeMacBook-Air.local>
2026-04-08 02:10:32 +0900

    fxied __TIMESTAMP__ type

--- context.go
+++ context.go
@@ -148,7 +148,7 @@ func loadConfig(plugin unsafe.Pointer) (*Config, error) {
 
 	if v := get("FieldTypes"); v != "" {
 		cfg.FieldTypes = parseFieldTypes(v)
-		cfg.FieldTypes["__TIMESTAMP__"] = "timestamp"
+		cfg.FieldTypes["__TIMESTAMP__"] = "timestamp_milli"
 		cfg.FieldTypes["__TAG__"] = "string"
 		log.Printf("[parquet] FieldTypes=%v \n", cfg.FieldTypes)
 	}
